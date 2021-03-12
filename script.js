8805fe9a796270bcde39c0ded13381aa7725e527
MatasRu <matas.rudaminas@gmail.com>
2021-03-12 15:25:05 +0200

    working true words

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 let wordsPlace = document.getElementById(`wordsPlace`)
+let checkBtn = document.getElementById(`check`)
+const gallows = document.getElementsByClassName(`gallows`)
 
 let words = ["Labas", "Krabas", "Lietuvis", "Baisus", "Debesis"]
 let word = words[Math.floor(Math.random() * Math.floor(words.length))]
-console.log(word)
+
+checkBtn.addEventListener(`click`, checkChar)
 
 
+gallows[0].innerHTML = `
+    <img src="img/gallows.jpg" alt="">
+`
 for (let i = 0; i < word.length; i++) {
     wordsPlace.innerHTML += `
-        <div class="symbol"><span class="false" id="${i}">${word[i]}</span></div>
+        <div class="symbol"><span class="false" id="${word[i].toLowerCase()}">${word[i]}</span></div>
     `
 }
+
+function checkChar() {
+    let choosedChar = symbol.value
+    let symbols = document.getElementsByClassName(`false`)
+    for (let i = 0; i < word.length; i++) {
+        if (choosedChar === symbols[i].id) {
+            symbols[i].classList.add("TRUE")
+            console.log('true')
+        }
+    }
+
+}
+
